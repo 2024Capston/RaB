77986e5082c122bd164951d43cee6c477a4c5ab8
CH2MI <gwanwoopa@naver.com>
2025-01-09 16:09:58 +0900

    #8 LobbyManager에서 사용할 주요 메소드 생성

--- Assets/Scripts/Lobby/LobbyManager.cs
+++ Assets/Scripts/Lobby/LobbyManager.cs
@@ -31,10 +31,55 @@ public class LobbyManager : NetworkSingletonBehaviour<LobbyManager>
         LobbyUIController.SetPlayerColorData(IsHost);
         
         SpawnPlayerServerRpc();
+
         
         UIManager.Instance.CloseAllOpenUI();
     }
 
+    /// <summary>
+    /// Stage 문을 열때 호출하는 메소드
+    /// </summary>
+    /// <param name="stageName"></param>
+    [ServerRpc(RequireOwnership = false)]
+    public void RequestOpenDoorServerRpc(StageName stageName)
+    {
+        
+    }
+
+    /// <summary>
+    /// Elevator 문을 열때 호출하는 메소드
+    /// </summary>
+    [ServerRpc(RequireOwnership = false)]
+    public void RequestOpenElevatorServerRpc()
+    {
+        
+    }
+
+    /// <summary>
+    /// 엘리베이터에서 층을 이동할 때 호출하는 메소드
+    /// </summary>
+    [ServerRpc(RequireOwnership = false)]
+    public void RequestMoveFloorServerRpc(int floor)
+    {
+        
+    }
+    
+    /// <summary>
+    /// InGame으로 이동하는 메소드 
+    /// </summary>
+    /// <param name="stageName"></param>
+    [ServerRpc(RequireOwnership = false)]
+    public void RequestPlayStageServerRpc(StageName stageName)
+    {
+        // InGame Scene으로 이동하면 ConnectionManager에 있는 SelectStage로 Loader를 불러온다.
+        SessionManager.Instance.SelectedStage = stageName;
+        NetworkManager.SceneManager.LoadScene(SceneType.InGame.ToString(), UnityEngine.SceneManagement.LoadSceneMode.Single);
+    }
+    
+    /// <summary>
+    /// PlayerConfig를 참고하여 
+    /// </summary>
+    /// <param name="serverRpcParams"></param>
     [ServerRpc(RequireOwnership = false)]
     private void SpawnPlayerServerRpc(ServerRpcParams serverRpcParams = default)
     {
@@ -48,16 +93,10 @@ public class LobbyManager : NetworkSingletonBehaviour<LobbyManager>
         player.GetComponent<NetworkObject>().SpawnWithOwnership(serverRpcParams.Receive.SenderClientId);
         playerConfig.MyPlayer = player.GetComponent<PlayerController>();
     }
-    
-    /// <summary>
-    /// InGame으로 이동하는 메소드 
-    /// </summary>
-    /// <param name="stageName"></param>
-    [ServerRpc]
-    public void LoadInGameServerRpc(StageName stageName)
+
+    [ClientRpc]
+    private void SetMapDataClientRpc()
     {
-        // InGame Scene으로 이동하면 ConnectionManager에 있는 SelectStage로 Loader를 불러온다.
-        SessionManager.Instance.SelectedStage = stageName;
-        NetworkManager.SceneManager.LoadScene(SceneType.InGame.ToString(), UnityEngine.SceneManagement.LoadSceneMode.Single);
+        
     }
 }
