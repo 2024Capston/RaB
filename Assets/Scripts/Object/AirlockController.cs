6507e435274b31b258a08dbc89190356f9632d9a
CH2MI <gwanwoopa@naver.com>
2025-01-16 14:25:56 +0900

    #8 Airlock Light 설정

--- Assets/Scripts/Object/AirlockController.cs
+++ Assets/Scripts/Object/AirlockController.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.Netcode;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class AirlockController : NetworkBehaviour
 {
@@ -9,19 +10,29 @@ public class AirlockController : NetworkBehaviour
     [SerializeField] private DoorController _doorOut;
 
     /// <summary>
-    /// 0 : off , 1 : Blue, 2 : Red Material
+    /// 0 : Lock Material, 1 : Open Material
     /// </summary>
-    [SerializeField] Material[] _inoutMaterials = new Material[3];
+    [SerializeField] private Material[] _doorLightMaterials = new Material[2];
+
+    /// <summary>
+    /// 0 : _doorIn Mesh, 1 : _doorOut Mesh
+    /// </summary>
+    [SerializeField] private MeshRenderer[] _doorLightMeshRenderers = new MeshRenderer[2];
+    
+    /// <summary>
+    /// 0 : off Material, 1 : Blue Material, 2 : Red Material
+    /// </summary>
+    [SerializeField] private Material[] _inoutMaterials = new Material[3];
     
     /// <summary>
     /// 0 : blue In Mesh, 1 : blue Out Mesh
     /// </summary>
-    [SerializeField] MeshRenderer[] _blueInOutMaterials = new MeshRenderer[2];
+    [SerializeField] private MeshRenderer[] _blueInOutMeshRenderers = new MeshRenderer[2];
     
     /// <summary>
     /// 0 : red In Mesh, 1 : red Out Mesh
     /// </summary>
-    [SerializeField] MeshRenderer[] _redInOutMaterials = new MeshRenderer[2];
+    [SerializeField] private MeshRenderer[] _redInOutMeshRenderers = new MeshRenderer[2];
     
     // 두 값이 true일 땐 DoorIn이 개방, 두 값이 false일 땐 DoorOut이 개방
     private bool _isBlueOpened = true;
@@ -38,23 +49,27 @@ public class AirlockController : NetworkBehaviour
         set
         {
             _isInOpened = value;
-            _doorIn.IsOpened = _isInOpened && IsOpened;
-            _doorOut.IsOpened = !_isInOpened && IsOpened;
+            _doorIn.IsOpened = _isInOpened && IsAirlockOpened;
+            _doorOut.IsOpened = !_isInOpened && IsAirlockOpened;
+            _doorLightMeshRenderers[0].material = _doorLightMaterials[_doorIn.IsOpened ? 1 : 0];
+            _doorLightMeshRenderers[1].material = _doorLightMaterials[_doorOut.IsOpened ? 1 : 0];
         }
     }
 
-    private bool _isOpened;
+    private bool _isAirlockOpened;
     /// <summary>
     /// true일 때 스테이지가 해금 되어 해당 문에 접근할 수 있다.
     /// </summary>
-    public bool IsOpened
+    public bool IsAirlockOpened
     {
-        get => _isOpened;
+        get => _isAirlockOpened;
         set
         {
-            _isOpened = value;
-            _doorIn.IsOpened = _isOpened && _isInOpened;
-            _doorOut.IsOpened = _isOpened && !_isInOpened;
+            _isAirlockOpened = value;
+            _doorIn.IsOpened = _isAirlockOpened && _isInOpened;
+            _doorOut.IsOpened = _isAirlockOpened && !_isInOpened;
+            _doorLightMeshRenderers[0].material = _doorLightMaterials[_doorIn.IsOpened ? 1 : 0];
+            _doorLightMeshRenderers[1].material = _doorLightMaterials[_doorOut.IsOpened ? 1 : 0];
         }
     }
     public StageName StageName { get; set; }
@@ -62,7 +77,7 @@ public class AirlockController : NetworkBehaviour
     public override void OnNetworkSpawn()
     {
         StageName = StageName.Size;
-        IsOpened = true;
+        IsAirlockOpened = true;
         IsInOpened = true;
 
         // Init
@@ -73,7 +88,6 @@ public class AirlockController : NetworkBehaviour
     [ServerRpc(RequireOwnership = false)]
     public void OnClickAirlockButtonServerRpc(ColorType colorType, bool isInButton)
     {
-        Logger.Log($"Colortype {colorType}, is in : {isInButton}");
         SetButtonMaterialClientRpc(colorType, isInButton);
         if (colorType == ColorType.Blue)
         {
@@ -88,6 +102,12 @@ public class AirlockController : NetworkBehaviour
         if (_isBlueOpened == _isRedOpened)
         {
             IsInOpened = _isBlueOpened;
+            
+            // TODO 둘 다 true가 되었을 때 Stage 이동이 필요
+            /*if (!_isBlueOpened)
+            {
+                LobbyManager.Instance.RequestPlayStageServerRpc(StageName);
+            }*/
         }
     }
 
@@ -96,13 +116,13 @@ public class AirlockController : NetworkBehaviour
     {
         if (colorType == ColorType.Blue)
         {
-            _blueInOutMaterials[isInButton ? 0 : 1].material = _inoutMaterials[1];
-            _blueInOutMaterials[isInButton ? 1 : 0].material = _inoutMaterials[0];
+            _blueInOutMeshRenderers[isInButton ? 0 : 1].material = _inoutMaterials[1];
+            _blueInOutMeshRenderers[isInButton ? 1 : 0].material = _inoutMaterials[0];
         }
         else
         {
-            _redInOutMaterials[isInButton ? 0 : 1].material = _inoutMaterials[2];
-            _redInOutMaterials[isInButton ? 1 : 0].material = _inoutMaterials[0];
+            _redInOutMeshRenderers[isInButton ? 0 : 1].material = _inoutMaterials[2];
+            _redInOutMeshRenderers[isInButton ? 1 : 0].material = _inoutMaterials[0];
         }
     }
 }
