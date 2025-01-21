c5cf4a326ade5f922b0d4f626801b99f081d4060
mirinae123 <137975323+mirinae123@users.noreply.github.com>
2025-01-21 21:20:16 +0900

    URP로 갱신

--- Assets/Scripts/Objects/Interactables/CubeRenderer.cs
+++ Assets/Scripts/Objects/Interactables/CubeRenderer.cs
@@ -1,15 +1,67 @@
+using System.Runtime.CompilerServices;
+using Unity.VisualScripting;
 using UnityEngine;
+using Unity.Netcode;
 
-public class CubeRenderer : MonoBehaviour
+public class CubeRenderer : NetworkBehaviour
 {
-    [SerializeField] Material[] _cubeMaterials;
+    [SerializeField] Material[] _materials;
 
     private CubeController _cubeController;
+    private NetworkInterpolator _networkInterpolator;
 
-    void Start()
+    private Transform[] _piecesTransforms;
+    private MeshRenderer[] _piecesMeshRenderers;
+
+    private void Start()
     {
         _cubeController = GetComponent<CubeController>();
+        _networkInterpolator = GetComponent<NetworkInterpolator>();
 
-        GetComponent<MeshRenderer>().material = _cubeMaterials[(int)_cubeController.CubeColor - 1];
+        _networkInterpolator.AddVisualReferenceDependantFunction(() =>
+        {
+            int childCount = _networkInterpolator.VisualReference.transform.childCount;
+            _piecesTransforms = new Transform[childCount];
+            _piecesMeshRenderers = new MeshRenderer[childCount];
+
+            for (int i = 0; i < childCount; i++)
+            {
+                Transform child = _networkInterpolator.VisualReference.transform.GetChild(i);
+
+                _piecesTransforms[i] = child.transform;
+                _piecesMeshRenderers[i] = child.GetComponent<MeshRenderer>();
+
+                Material[] materials = _piecesMeshRenderers[i].materials;
+                materials[1] = _materials[(int)_cubeController.CubeColor - 1];
+                _piecesMeshRenderers[i].materials = materials;
+            }
+        });
+    }
+
+    [ServerRpc(RequireOwnership = false)]
+    private void UpdateColorServerRpc()
+    {
+        UpdateColorClientRpc();
+    }
+
+    [ClientRpc(RequireOwnership = false)]
+    private void UpdateColorClientRpc()
+    {
+        int childCount = _networkInterpolator.VisualReference.transform.childCount;
+
+        for (int i = 0; i < childCount; i++)
+        {
+            Material[] materials = _piecesMeshRenderers[i].materials;
+            materials[1] = _materials[(int)_cubeController.CubeColor - 1];
+            _piecesMeshRenderers[i].materials = materials;
+        }
+    }
+
+    /// <summary>
+    /// 큐브의 렌더링 색깔을 갱신한다.
+    /// </summary>
+    public void UpdateColor()
+    {
+        UpdateColorServerRpc();
     }
 }
