39666cf1cf45116213791f11ebce38d83390efb5
CH2MI <gwanwoopa@naver.com>
2025-02-27 17:05:47 +0900

    #14 CodeInputUI 제작

--- Assets/Scripts/UI/CodeInputUI.cs
+++ Assets/Scripts/UI/CodeInputUI.cs
@@ -1,33 +1,41 @@
+using System;
+using System.Runtime.CompilerServices;
 using Steamworks;
 using TMPro;
 using UnityEngine;
-using UnityEngine.UI;
+using UnityEngine.UIElements;
 
-public class CodeInputUI : BaseUI
+public class CodeInputUI
 {
-    [SerializeField]
-    private TMP_InputField _inputField;
+    private VisualElement _root;
 
-    [SerializeField]
+    private TextField _codeTextField;
+    private Label _descText;
     private Button _okButton;
+    private Button _backButton;
 
-    [SerializeField]
-    private TMP_Text _descText;
-
-    public override void SetInfo(BaseUIData uiData)
+    private Action OnClosePanel;
+    public CodeInputUI(VisualElement root, Action OnCloseCodeInputUI)
     {
-        base.SetInfo(uiData);
-
+        _root = root;
+        _codeTextField = _root.Q<TextField>("CodeTextField");
+        _descText = _root.Q<Label>("DescText");
+        _okButton = _root.Q<Button>("OK_Button");
+        _backButton = _root.Q<Button>("Back_Button");
+        
         _descText.text = "";
+        OnClosePanel = OnCloseCodeInputUI;
+        _okButton.clicked += OnClickOkButton;
+        _backButton.clicked += OnClickBackButton;
     }
-
+    
     /// <summary>
     ///  버튼을 눌렀을 때 일치하는 Lobby가 있는지 확인하고, 해당하는 Lobby가 있으면 입장한다.
     /// </summary>
     public async void OnClickOkButton()
     {
         // 코드를 입력받고 그에 따른 처리를 합니다.
-        Result result = await RaB.Connection.ConnectionManager.Instance.StartClient(_inputField.text);
+        Result result = await RaB.Connection.ConnectionManager.Instance.StartClient(_codeTextField.text);
 
         switch (result)
         {
@@ -35,36 +43,36 @@ public class CodeInputUI : BaseUI
             case Result.OK:
             {
                 _descText.text = "Find the lobby!";
-                _descText.color = UnityEngine.Color.black;
-                CloseUI();
+                _descText.style.color = UnityEngine.Color.white;
+                OnClosePanel?.Invoke();
                 break;
             }
             // 코드 형식이 올바르지 않은 경우
             case Result.InvalidParam:
             {
                 _descText.text = "The format is incorrect.";
-                _descText.color = UnityEngine.Color.red;
+                _descText.style.color = UnityEngine.Color.red;
                 break;
             }
             // 로비가 꽉 찾을 때
             case Result.Busy:
             {
                 _descText.text = "Lobby is full";
-                _descText.color = UnityEngine.Color.red;
+                _descText.style.color = UnityEngine.Color.red;
                 break;
             }
             // 유효하지 않은 코드
             case Result.InvalidName:
             {
                 _descText.text = "Couldn't find the lobby";
-                _descText.color = UnityEngine.Color.red;
+                _descText.style.color = UnityEngine.Color.red;
                 break;
             }
         }
     }
 
-    public void OnClickUndoButton()
+    public void OnClickBackButton()
     {
-        CloseUI();
+        OnClosePanel?.Invoke();
     }
 }
