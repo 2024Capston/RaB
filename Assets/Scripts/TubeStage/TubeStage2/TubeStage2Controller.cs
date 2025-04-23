bcf5219d589cd97eb6362729dea9ac9a516a9b4f
CH2MI <gwanwoopa@naver.com>
2025-04-23 18:10:53 +0900

    #25 Stage2 시나리오 완성

--- Assets/Scripts/TubeStage/TubeStage2/TubeStage2Controller.cs
+++ Assets/Scripts/TubeStage/TubeStage2/TubeStage2Controller.cs
@@ -1,16 +1,25 @@
+using System.Collections.Generic;
+using UnityEngine;
+
 namespace TubeStage
 {
     public class TubeStage2Controller : TubeStageController
     {
+        /// <summary>
+        /// Stage를 초기 상태로 만든다.
+        /// </summary>
         public void InitStage()
         {
-            for (int i = 0; i < _sourceTubeGroup.Count; i++)
+            // source tube의 모든 value를 0f로 만든다.
+            foreach (var tube in _sourceTubeGroup)
             {
-                _sourceTubeGroup[i].UpdateValue(-1f);
+                tube.UpdateValue(-1f);
             }
             
-            _destTube.UpdateValue(-1f);
+            // destination tube의 모든 value를 0f로 만든다.
+            _destinationTube.UpdateValue(-1f);
 
+            // source button을 모두 비활성화 한다.
             for (int i = 0; i < _sourceButtonGroup.Count; i++)
             {
                 SetSourceButtonEnable(i, false);
@@ -18,12 +27,80 @@ namespace TubeStage
                 _sourceButtonGroup[i].SetButtonColor(ColorType.None);
             }
             
-            _descButton.SetButtonColor(ColorType.Purple);
-            _descButton.UnpressButton();
-            _descButton.PlayPressAnimation(false);
-            _descButton.EnableButton();
+            // destination button을 활성화한다.
+            _destinationButton.SetButtonColor(ColorType.Purple);
+            _destinationButton.UnpressButton();
+            _destinationButton.PlayPressAnimation(false);
+            _destinationButton.EnableButton();
             
-            // 모니터 설정하기
+            // TODO 모니터 설정하기
+        }
+
+        /// <summary>
+        /// 게임이 시작되었을 때 Tube와 Button에 색 배정을 한다.
+        /// </summary>
+        /// <param name="colorList"></param>
+        public void StartStage(List<ColorType> colorList)
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                _sourceTubeGroup[i].SetTubeColorClientRpc(colorList[i]);
+                _sourceTubeGroup[i].UpdateValue(1f);
+                _sourceButtonGroup[i].SetButtonColor(colorList[i]);
+                
+                Logger.Log($"colorList {i} : {colorList[i]}");
+            }
+        }
+
+        /// <summary>
+        /// Stage의 정답 입력을 다시 재개
+        /// </summary>
+        public void ResumeStage()
+        {
+            // source button을 모두 활성화 한다.
+            for (int i = 0; i < _sourceButtonGroup.Count; i++)
+            {
+                SetSourceButtonEnable(i, true);
+                SetSourceButtonPress(i, false);
+            }
+            
+            // destination button을 비활성화한다.
+            _destinationButton.SetButtonColor(ColorType.None);
+            _destinationButton.PressButton();
+            _destinationButton.PlayPressAnimation(true);
+            _destinationButton.DisableButton();
+        }
+        
+        /// <summary>
+        /// Stage에서 정답 입력을 중지
+        /// </summary>
+        public void StopStage() 
+        {
+            // source button을 모두 활성화 한다.
+            for (int i = 0; i < _sourceButtonGroup.Count; i++)
+            {
+                SetSourceButtonEnable(i, false);
+                SetSourceButtonPress(i, true);
+            }
+            
+            // destination button을 활성화한다.
+            _destinationButton.SetButtonColor(ColorType.Purple);
+            _destinationButton.UnpressButton();
+            _destinationButton.PlayPressAnimation(false);
+            _destinationButton.EnableButton();
+        }
+
+        public void ApplyProblem(ColorType colorType)
+        {
+            // 모니터에 띄우기
+            Logger.Log($"Problem {colorType}");
+        }
+
+        public void ApplyTubeCondition(ColorType colorType)
+        {
+            TubeController tube = _sourceTubeGroup.Find(x => x.Color == colorType);
+            tube.UpdateValue(-0.33333f);
+            _destinationTube.UpdateValue(0.125f);
         }
     }
 }
--- Assets/Scripts/TubeStage/TubeStage2/WaitingState.cs
+++ Assets/Scripts/TubeStage/TubeStage2/WaitingState.cs
@@ -1,36 +1,60 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
 namespace TubeStage
 {
     /// <summary>
     /// 게임 시작을 기다리고 있는 State
-    /// Player가 DescButton을 누르면 AskingState로 넘어갑니다.
+    /// Player가 DestinationButton을 누르면 AskingState로 넘어갑니다.
     /// </summary>
-    public class WaitingState : TubeStageState
+    internal class WaitingState : TubeStageState
     {
+        /// <summary>
+        /// Waiting State에 진입하면 모든 Object가 기본값으로 초기화 
+        /// </summary>
         public override void Enter()
         {
-            /*
-             * TODO
-             * 1. SourceTube.Fill을 0으로 만든다.
-             * 2. DescTube.Fill을 0으로 만든다.
-             * 3. SourceButton을 모두 비활성화한다.
-             * 4. DescButton을 활성화한다.
-             * 5. Monitor에 PressStart를 띄운다.
-             */
+            TubeStage2Manager.Instance.TubeStageController.InitStage();
         }
 
+        /// <summary>
+        /// Asking State로 넘어갈 때엔 문제를 새로 생성하고 색 배치를 새로 만듭니다.
+        /// </summary>
         public override void Exit()
         {
-            // TODO
-            // 문제를 만든다.
+            TubeStage2Manager.Instance.Problem = MakingProblem();
             
+            List<ColorType> colorList = new List<ColorType>
+                { ColorType.None, ColorType.Blue, ColorType.Red, ColorType.Purple };
+            Random random = new Random();
             
+            TubeStage2Manager.Instance.TubeStageController.StartStage(colorList.OrderBy(x => random.Next()).ToList());
         }
 
+        /// <summary>
+        /// 버튼이 눌리면 Asking State로 전환
+        /// </summary>
+        /// <param name="colorType"></param>
         public override void OnButtonClicked(ColorType colorType)
         {
-            // TODO
-            // 현재 작동 가능한 버튼은 DescButton 밖에 없다.
-            // Progress State로 넘어가면 된다.
+            TubeStage2Manager.Instance.ChangeState(TubeStage2Manager.Instance.Asking);
         }
+
+        private TubeStage2Problem MakingProblem()
+        {
+            List<int> answer = new List<int> { 0, 0, 0, 1, 1, 1, 2, 2, 2, 3, 3, 3 };
+            Random random = new Random();
+            
+            TubeStage2Problem problem = new TubeStage2Problem()
+            {
+                Answer = answer.OrderBy(x => random.Next()).Take(8).ToList(),
+                CurrentCount = 0,
+                FailCount = 0,
+                MaxFailCount = 3
+            };
+
+            return problem;
+        }   
     }
 }
