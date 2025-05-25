8d190075be0916b6cbfc390e84f8d440af37d7ab
Mirinae <137975323+mirinae123@users.noreply.github.com>
2025-05-25 21:08:23 +0900

    Issue: #44 필요한 파일 업로드 및 불필요한 파일 정리

---
+++ Assets/Scripts/Stage/StageManager/ColorChanger/CC2_StageManager.cs
@@ -0,0 +1,46 @@
+using Unity.Netcode;
+using UnityEngine;
+
+namespace ColorChanger
+{
+    public class CC2_StageManager : StageManager
+    {
+        [SerializeField] private AudioSource _clearAudiouSource;
+
+        public override void EndGame()
+        {
+            InGameManager.Instance.EndGameServerRpc();
+        }
+
+        public override void RestartGame()
+        {
+            EventBus.Instance.ClearEventBus();
+
+            foreach (PlayerController playerController in FindObjectsOfType<PlayerController>())
+            {
+                playerController.RespawnPlayer();
+                playerController.ForceStopInteraction();
+            }
+
+            foreach (NetworkObjectSpawner networkObjectSpawner in FindObjectsOfType<NetworkObjectSpawner>())
+            {
+                networkObjectSpawner.SpawnObject();
+            }
+
+            StartGame();
+        }
+
+        public override void StartGame()
+        {
+        }
+
+        [ClientRpc(RequireOwnership = false)]
+        private void PlayClearSoundClientRpc()
+        {
+            if (!_clearAudiouSource.isPlaying)
+            {
+                _clearAudiouSource.Play();
+            }
+        }
+    }
+}
