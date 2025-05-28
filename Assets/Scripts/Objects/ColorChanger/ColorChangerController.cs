3d83703a2d2ed042646b7f2b724a3a41c0142f54
Mirinae <137975323+mirinae123@users.noreply.github.com>
2025-05-29 03:07:06 +0900

    Issue: #44 색깔 변환기 게이지 색깔 변경

--- Assets/Scripts/Objects/ColorChanger/ColorChangerController.cs
+++ Assets/Scripts/Objects/ColorChanger/ColorChangerController.cs
@@ -28,6 +28,8 @@ namespace ColorChanger
         private Rigidbody _cubeRigidbody;
         private ColorChangerUtil _utilObject;
 
+        private Material[] _instancedMaterials;
+
         private float _timer;
 
         private void Awake()
@@ -160,8 +162,8 @@ namespace ColorChanger
             float fillTime = TRANSITION_TIME * 0.8f;
 
             Color initialColor = _gaugeMeshRenderer.material.GetColor("_FillColor");
-            Color originalColor = _materials[(int)cubeController.Color - 1].color;
-            Color newColor = _materials[2 - (int)cubeController.Color].color;
+            Color originalColor = _instancedMaterials[(int)cubeController.Color - 1].color;
+            Color newColor = _instancedMaterials[2 - (int)cubeController.Color].color;
 
             _gaugeMeshRenderer.material.SetColor("_BackgroundColor", originalColor);
 
@@ -207,6 +209,19 @@ namespace ColorChanger
         private void InitializeClientRpc(float changeTime)
         {
             _changeTime = changeTime;
+            _instancedMaterials = _materials;
+
+            if (PlayerController.LocalPlayer)
+            {
+                _instancedMaterials[2 - (int)PlayerController.LocalPlayer.Color].color = new Color(1.0f, 1.0f, 1.0f);
+            }
+            else
+            {
+                PlayerController.LocalPlayerCreated += () =>
+                {
+                    _instancedMaterials[2 - (int)PlayerController.LocalPlayer.Color].color = new Color(1.0f, 1.0f, 1.0f);
+                };
+            }
         }
     }
 }
